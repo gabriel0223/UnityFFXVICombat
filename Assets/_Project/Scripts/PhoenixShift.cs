5b8e3fa9a0b6ee05774dceb93b2df52b60fe2f40
Gabriel Dias <gabriel.dias0223@gmail.com>
2023-09-15 14:06:08 -0300

    Implement basic shift functionality

--- Assets/_Project/Scripts/PhoenixShift.cs
+++ Assets/_Project/Scripts/PhoenixShift.cs
@@ -1,11 +1,15 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using DG.Tweening;
 using UnityEngine;
 
 public class PhoenixShift : MonoBehaviour
 {
+    [SerializeField] private Material _projectionMaterial;
+    [SerializeField] private CinemachineImpulseSource _cameraImpulseSource;
+    [SerializeField] private GameObject _playerMesh;
     [SerializeField] private PlayerStateManager _playerStateManager;
     [SerializeField] private PlayerCombatController _combatController; 
     [SerializeField] private DashController _dashController;
@@ -31,54 +35,105 @@ public class PhoenixShift : MonoBehaviour
         }
     }
 
-    public void DisableMeshes()
+    public void ExecuteShift()
     {
+        DisableMeshes();
+        _cameraImpulseSource.GenerateImpulse();
+
+        _playerStateManager.SetPlayerState(PlayerState.Shifting);
+        
+        if (!_combatController.IsOnCombatMode)
+        {
+            _dashController.DashTowardsInput(_range, _dashDuration);
+        }
+        else
+        {
+            // EnemyHealth currentTarget = _combatController.CurrentTarget;
+            // float distanceToEnemy = Vector3.Distance(currentTarget.transform.position, transform.position);
+            // Vector3 enemyDirection = currentTarget.transform.position - transform.position;
+            // float dashDistance = _maxDashDistance;
+            //
+            // dashDistance = Mathf.Clamp(dashDistance, 0, _maxDashDistance);
+            //
+            // if (distanceToEnemy < _dashStoppingDistance)
+            // {
+            //     dashDistance = 0;
+            // }
+            //
+            // Vector3 dashDirection = enemyDirection * dashDistance;
+            // _dashController.DashTowardsDirection(dashDirection, 0.3f, true);
+        
+            EnemyHealth currentTarget = _combatController.CurrentTarget;
+            float distanceToEnemy = Vector3.Distance(currentTarget.transform.position, transform.position);
+            Vector3 enemyDirection = (currentTarget.transform.position - transform.position).normalized;
+            float dashDistance = distanceToEnemy * _distanceToEnemyMultiplier;
+        
+            dashDistance = Mathf.Clamp(dashDistance, 0, _range);
+            _dashController.DashTowardsDirection(enemyDirection * dashDistance, _dashDuration, true);
+        }
+        
+        DOVirtual.DelayedCall(_dashDuration, OnShiftEnd);
+    }
+
+    private void DisableMeshes()
+    {
+        SpawnProjection();
+
         foreach (Renderer mesh in _meshes)
         {
             mesh.enabled = false;
         }
+
+        _animator.speed = 0;
+        DOVirtual.DelayedCall(_dashDuration, EnableMeshes);
+    }
+
+    private void EnableMeshes()
+    {
+        _animator.speed = 1;
+
+        foreach (Renderer mesh in _meshes)
+        {
+            mesh.enabled = true;
+        }
+    }
+
+    private void OnShiftEnd()
+    {
+        _cameraImpulseSource.GenerateImpulse();
+        _playerStateManager.SetPlayerState(PlayerState.Idle);
+
+        AnimatePhoenixGlow(10, -10 ,0.5f);
     }
 
     public void OnPhoenixShift()
     {
         _animator.SetTrigger("PhoenixShift");
 
-        Color currentColor = _materialInstance.GetColor("_OutlineColor");
-        DOVirtual.Float(0, 10, 0.2f,
-            value => _materialInstance.SetColor("_OutlineColor", currentColor * Mathf.Pow(2, value)));
+        AnimatePhoenixGlow(0, 10 ,0.2f);
+    }
 
-        // _playerStateManager.SetPlayerState(PlayerState.Shifting);
-        //
-        // if (!_combatController.IsOnCombatMode)
-        // {
-        //     _dashController.DashTowardsInput(_range, _dashDuration);
-        // }
-        // else
-        // {
-        //     // EnemyHealth currentTarget = _combatController.CurrentTarget;
-        //     // float distanceToEnemy = Vector3.Distance(currentTarget.transform.position, transform.position);
-        //     // Vector3 enemyDirection = currentTarget.transform.position - transform.position;
-        //     // float dashDistance = _maxDashDistance;
-        //     //
-        //     // dashDistance = Mathf.Clamp(dashDistance, 0, _maxDashDistance);
-        //     //
-        //     // if (distanceToEnemy < _dashStoppingDistance)
-        //     // {
-        //     //     dashDistance = 0;
-        //     // }
-        //     //
-        //     // Vector3 dashDirection = enemyDirection * dashDistance;
-        //     // _dashController.DashTowardsDirection(dashDirection, 0.3f, true);
-        //
-        //     EnemyHealth currentTarget = _combatController.CurrentTarget;
-        //     float distanceToEnemy = Vector3.Distance(currentTarget.transform.position, transform.position);
-        //     Vector3 enemyDirection = (currentTarget.transform.position - transform.position).normalized;
-        //     float dashDistance = distanceToEnemy * _distanceToEnemyMultiplier;
-        //
-        //     dashDistance = Mathf.Clamp(dashDistance, 0, _range);
-        //     _dashController.DashTowardsDirection(enemyDirection * dashDistance, _dashDuration, true);
-        // }
-        //
-        // DOVirtual.DelayedCall(_dashDuration, () => _playerStateManager.SetPlayerState(PlayerState.Idle));
+    private void AnimatePhoenixGlow(float startValue, float endValue, float duration)
+    {
+        Color currentColor = _materialInstance.GetColor("_OutlineColor");
+
+        DOVirtual.Float(startValue, endValue, duration,
+            value => _materialInstance.SetColor(
+                "_OutlineColor", currentColor * Mathf.Pow(2, value)));
+    }
+
+    private void SpawnProjection()
+    {
+        GameObject projection = Instantiate(_playerMesh, _playerMesh.transform.position, _playerMesh.transform.rotation);
+        projection.transform.parent = null;
+
+        Renderer[] projectionMeshes = projection.GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer mesh in projectionMeshes)
+        {
+            mesh.material = _projectionMaterial;
+        }
+
+        projection.AddComponent<ProjectionController>();
     }
 }
---
+++ Assets/_Project/Scripts/ProjectionController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class ProjectionController : MonoBehaviour
+{
+    private Material _materialInstance;
+    private Renderer[] _renderers;
+
+    private void Start()
+    {
+        _renderers = GetComponentsInChildren<Renderer>();
+        _materialInstance = new Material(_renderers[0].material);
+
+        foreach (Renderer mesh in _renderers)
+        {
+            mesh.material = _materialInstance;
+        }
+
+        Sequence projectionSequence = DOTween.Sequence();
+        projectionSequence.AppendInterval(1f);
+        projectionSequence.Append(DOVirtual.Float(1, 0, 1f,
+            value => _materialInstance.SetFloat("_Alpha", value)));
+        projectionSequence.AppendCallback(() => Destroy(gameObject));
+    }
+}
